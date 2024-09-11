863d76d4fe137918cbf8a72cf0496dda32afbf9f
Jorge Daniel García Girón <jorge10d@gmail.com>
2024-09-11 11:51:12 -0600

    upload
    upload data for local

---
+++ pointclouds/portal/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2847033,
+    "boundingBox": {
+        "lx": 329937.76,
+        "ly": 1091855.94,
+        "lz": -1.17,
+        "ux": 330943.16000000006,
+        "uy": 1092861.3399999999,
+        "uz": 1004.2300000000233
+    },
+    "tightBoundingBox": {
+        "lx": 329937.76,
+        "ly": 1091855.94,
+        "lz": -1.17,
+        "ux": 330943.16000000006,
+        "uy": 1092809.84,
+        "uz": 141.25
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 8.707019805908204,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
